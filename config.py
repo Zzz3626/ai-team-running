9e256ca44b8ab248c98e4d0b3172447a98c15097
Zzz3626 <220018701022@lut.edu.cn>
2025-04-13 20:07:45 +0800

    first commit

---
+++ config.py
@@ -0,0 +1,8 @@
+# 存放API密钥、模型名称、游戏设定常量等
+LLM_API_KEY = "YOUR_LLM_API_KEY"
+IMAGE_API_KEY = "YOUR_IMAGE_API_KEY"
+LLM_MODEL_NAME = "gpt-4o-mini" # Or other model
+IMAGE_MODEL_NAME = "dall-e-3" # Or other model
+MAP_OUTPUT_PATH = "maps/"
+SAVE_GAME_PATH = "saves/"
+# ... 其他配置
